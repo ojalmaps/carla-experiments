779c3c2b65f2d56790f65f4dc5482a9084ec02ae
unknown <carla@PARTICLE.ad.crm.ucsc.edu>
2022-04-24 18:40:37 -0700

    updated joj model from static

--- agents/pedestrians/speed_models/jojSpeedModel.py
+++ agents/pedestrians/speed_models/jojSpeedModel.py
@@ -6,4 +6,5 @@ class jojSpeedModel(SpeedModel):
         self._desiredSpeed = self.internalFactors["desired_speed"] * 3
         self._minSpeed = self.internalFactors["min_crossing_speed"] * 3
         self._maxSpeed = self.internalFactors["max_crossing_speed"] * 3
+        self._relaxationTime = self.internalFactors["relaxation_time"]
         pass
