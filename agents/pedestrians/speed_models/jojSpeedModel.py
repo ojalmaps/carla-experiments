da2c05014fd9c8f58989a5a25e1f7dd4007bac2b
unknown <carla@PARTICLE.ad.crm.ucsc.edu>
2022-04-24 17:54:12 -0700

    car hit pedestrian in this configuration where relaxation time is one and desired speed is 6 and the internal factors are multiplied by 3, min speed 5 and max speed 10

---
+++ agents/pedestrians/speed_models/jojSpeedModel.py
@@ -0,0 +1,9 @@
+from .SpeedModel import SpeedModel
+
+class jojSpeedModel(SpeedModel):
+    
+    def initialize(self):
+        self._desiredSpeed = self.internalFactors["desired_speed"] * 3
+        self._minSpeed = self.internalFactors["min_crossing_speed"] * 3
+        self._maxSpeed = self.internalFactors["max_crossing_speed"] * 3
+        pass
