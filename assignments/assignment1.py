61a4f167999889042e6e3cf15802501feb0507b0
unknown <carla@PARTICLE.ad.crm.ucsc.edu>
2022-05-06 15:19:29 -0700

    Finally adding pedestrian

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -14,17 +14,20 @@ map = world.get_map()
 spawn_points = map.get_spawn_points()
 spawn_point = spawn_points[0]  # Choosing 1 spawn point
 
-
 blueprint_library = world.get_blueprint_library()
 # print(blueprint_library)
-#vehicle = blueprint_library.filter('vehicle.audi.a2')
-walker = blueprint_library.filter('walker.pedestrian.')
-world.spawn_actor(walker, spawn_point)
+walker = blueprint_library.filter('walker.*')
+walker = walker[0]
+print(walker)
+
+pedestrain = world.spawn_actor(walker, spawn_point)
 
 blueprint = world.get_blueprint_library().find('sensor.camera.rgb')
+walker_controller_bp = world.get_blueprint_library().find('controller.ai.walker')
+
 # Modify the attributes of the blueprint to set image resolution and field of view.
-blueprint.set_attribute('image_size_x', '1920')
-blueprint.set_attribute('image_size_y', '1080')
-blueprint.set_attribute('fov', '110')
-# Set the time in seconds between sensor captures
-blueprint.set_attribute('sensor_tick', '1.0')
+# blueprint.set_attribute('image_size_x', '1920')
+# blueprint.set_attribute('image_size_y', '1080')
+# blueprint.set_attribute('fov', '110')
+# # Set the time in seconds between sensor captures
+# blueprint.set_attribute('sensor_tick', '1.0')
