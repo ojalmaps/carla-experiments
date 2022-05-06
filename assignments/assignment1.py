b58b529dd0ba4bbc34c25f6c38ac0b321c6e98cd
unknown <carla@PARTICLE.ad.crm.ucsc.edu>
2022-05-06 16:43:26 -0700

    adding perspective

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -7,9 +7,9 @@ from numpy import random
 client = carla.Client('127.0.0.1', 2000)  # specify the IP of the host and port
 client.set_timeout(10.0)
 print(client.get_client_version())
-
 # print(client.get_available_maps())
-world = client.load_world("circle_t_junctions")  # loading
+
+world = client.load_world("circle_t_junctions")  # Loading the  world
 map = world.get_map()
 spawn_points = map.get_spawn_points()
 spawn_point = spawn_points[0]  # Choosing 1 spawn point
@@ -17,17 +17,18 @@ spawn_point = spawn_points[0]  # Choosing 1 spawn point
 blueprint_library = world.get_blueprint_library()
 # print(blueprint_library)
 walker = blueprint_library.filter('walker.*')
-walker = walker[0]
-print(walker)
-
+walker = walker[0]  # Choosing first walker 
 pedestrain = world.spawn_actor(walker, spawn_point)
 
-blueprint = world.get_blueprint_library().find('sensor.camera.rgb')
-walker_controller_bp = world.get_blueprint_library().find('controller.ai.walker')
+player_control = carla.WalkerControl()
+player_control.speed = 3
+pedestrain.apply_control(player_control)
 
-# Modify the attributes of the blueprint to set image resolution and field of view.
-# blueprint.set_attribute('image_size_x', '1920')
-# blueprint.set_attribute('image_size_y', '1080')
-# blueprint.set_attribute('fov', '110')
-# # Set the time in seconds between sensor captures
-# blueprint.set_attribute('sensor_tick', '1.0')
+spectator = world.get_spectator()  # Gets spectator from the world 
+# Creating a new transformation to place the spectator
+rotation = carla.Rotation()
+new_location = carla.Location( x = -135, y =-30, z= 3)
+new_transform = carla.Transform(new_location, rotation)
+spectator.set_transform(new_transform)
+# spectator.set_location(spawn_point)
+# Notes Transform objects = Location and Transform 
